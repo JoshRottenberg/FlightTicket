588ee9bfe4a6db6a1fda32ec998ae5213dfd0dd4
Pini <pinikaimer8@gmail.com>
2023-06-26 10:19:33 +0300

    update flights

--- flights.py
+++ flights.py
@@ -12,18 +12,28 @@ def flight_found(origin_city, destination_city, departure_date, num_of_seats):
     origin_city = origin_city
     dest_city = destination_city
 
-
     # Select rows that match the search criteria
     query = f"SELECT * FROM {table_name} WHERE FLIGHTDATE = ? AND ORIGINCITYNAME = ? AND DESTCITYNAME = ?"
     cursor.execute(query, (flight_date, origin_city, dest_city))
     rows = cursor.fetchall()
+
+    for row in rows:
+        seats_avel = row[-1]
+        if num_of_seats < seats_avel:
+            print("Flight Details:")
+            #print(f"Transaction ID: {row[0]}")
+            print(f"Flight Date: {row[1]}")
+            print(f"Airline Name: {row[3]}")
+            print(f"Origin Airport: {row[6]}")
+            print(f"Destination Airport: {row[11]}")
+            print(f"Departure Time: {row[14][:2] + ':' + row[14][2:]}")
+            print(f"Arrival Time: {row[15][:2] + ':' + row[15][2:]}")
+            #print(f"Seats Available: {row[-1]}")
+            print()
+
     connection.close()
 
     if rows:
-        for row in rows:
-            print(row)
         return True
     else:
         return False
-
-
