75fb12fc564dde55937a6625e7d812aa377958bf
Pini <pinikaimer8@gmail.com>
2023-06-26 21:16:55 +0300

    update db

---
+++ create_db.py
@@ -0,0 +1,73 @@
+# import sqlite3
+#
+# # Create a connection to the database file
+# conn = sqlite3.connect('big_data.db')
+#
+# # Create a cursor object to execute SQL commands
+# cursor = conn.cursor()
+#
+# # Create the "users" table with the desired columns
+# cursor.execute('''
+#     CREATE TABLE IF NOT EXISTS users (
+#         id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         first_name TEXT,
+#         last_name TEXT,
+#         phone_num TEXT,
+#         email TEXT,
+#         password TEXT,
+#         is_signed_up BOOL
+#     )
+# ''')
+#
+# # Commit the changes and close the connection
+# conn.commit()
+# conn.close()
+#
+#
+#
+# import sqlite3
+#
+# # Create a connection to the database file
+# conn = sqlite3.connect('big_data.db')
+#
+# # Create a cursor object to execute SQL commands
+# cursor = conn.cursor()
+#
+# # Create the "users" table with the desired columns
+# cursor.execute('''
+#     CREATE TABLE IF NOT EXISTS orders (
+#         id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         user_id UNSIGNED,
+#         num_of_tickets UNSIGNED,
+#         total_price UNSIGNED
+#     )
+# ''')
+#
+# # Commit the changes and close the connection
+# conn.commit()
+# conn.close()
+#
+#
+#
+# import sqlite3
+#
+# # Create a connection to the database file
+# conn = sqlite3.connect('big_data.db')
+#
+# # Create a cursor object to execute SQL commands
+# cursor = conn.cursor()
+#
+# # Create the "users" table with the desired columns
+# cursor.execute('''
+#     CREATE TABLE IF NOT EXISTS passengers (
+#         id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         first_name TEXT,
+#         last_name TEXT,
+#         date_of_birth DATE,
+#         passport TEXT
+#     )
+# ''')
+#
+# # Commit the changes and close the connection
+# conn.commit()
+# conn.close()
