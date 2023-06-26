1df57d233378f1cc30599b49364fdb9c7e3f5cfe
Pini <pinikaimer8@gmail.com>
2023-06-26 12:44:21 +0300

    update db

--- print_flight_db.py
+++ print_flight_db.py
@@ -1,44 +1,17 @@
-# import sqlite3
-#
-# # Connect to the database
-# conn = sqlite3.connect('flights_base2.db')
-# cursor = conn.cursor()
-#
-# # Execute the SELECT query to retrieve the TRANSACTIONID column
-# cursor.execute("SELECT TRANSACTIONID FROM Flights LIMIT 150")
-# rows = cursor.fetchall()
-#
-# # Print the TRANSACTIONID for each row
-# for row in rows:
-#     transaction_id = row[0]
-#     print(transaction_id)
-#
-# # Close the connection
-# conn.close()
 import sqlite3
 
 # Connect to the database
-conn = sqlite3.connect('your_database.db')
+conn = sqlite3.connect('flights_base2.db')
 cursor = conn.cursor()
 
-# Execute the SELECT query to retrieve all TRANSACTIONID values
-cursor.execute("SELECT TRANSACTIONID FROM Flights")
+# Execute the SELECT query to retrieve the TRANSACTIONID column
+cursor.execute("SELECT price FROM Flights LIMIT 150")
 rows = cursor.fetchall()
 
-# Create a set to store unique TRANSACTIONID values
-unique_ids = set()
-
-# Iterate over the rows and check for duplicates
+# Print the TRANSACTIONID for each row
 for row in rows:
     transaction_id = row[0]
-    if transaction_id in unique_ids:
-        print(f"Duplicate TRANSACTIONID found: {transaction_id}")
-    else:
-        unique_ids.add(transaction_id)
-
-# If no duplicates found, print a success message
-if len(unique_ids) == len(rows):
-    print("All TRANSACTIONID values are unique.")
+    print(transaction_id)
 
 # Close the connection
 conn.close()
