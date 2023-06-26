f07e0cf88a1e74d1e838ed22d65007347dac6f26
Pini <pinikaimer8@gmail.com>
2023-06-26 09:01:57 +0300

    added a new column "SEATS_AVAILABLE"

--- print_user_db.py
+++ print_user_db.py
@@ -1,19 +1,25 @@
 import sqlite3
 
-# Connect to the database
-conn = sqlite3.connect('user_data.db')
-cursor = conn.cursor()
+try:
+    # Connect to the database
+    conn = sqlite3.connect('user_data.db')
+    cursor = conn.cursor()
 
-# Execute a SELECT query to retrieve all rows from the "users" table
-cursor.execute('SELECT * FROM users')
-rows = cursor.fetchall()
+    # Execute a SELECT query to retrieve all rows from the "users" table
+    cursor.execute('SELECT * FROM users')
+    rows = cursor.fetchall()
 
-# Print the column headers
-print("ID | First Name | Last Name | Phone Number | Email | Password | Is Signed Up")
+    # Print the column headers
+    print("ID | First Name | Last Name | Phone Number | Email | Password | Is Signed Up")
 
-# Iterate over the rows and print the data
-for row in rows:
-    print(row)
+    # Iterate over the rows and print the data
+    for row in rows:
+        print(row)
 
-# Close the connection
-conn.close()
+except sqlite3.Error as e:
+    print("Error accessing the database:", e)
+
+finally:
+    # Close the connection
+    if conn:
+        conn.close()
